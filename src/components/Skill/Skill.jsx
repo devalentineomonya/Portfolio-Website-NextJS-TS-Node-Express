f3154bd57378e518ee258caad6449c9a0fa434d3
Valentine Omonya <valomosh254@gmail.com>
2024-03-15 18:52:14 +0300

    Changed Hero section top and About section Paddding

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './skill.css';
+import React from "react";
+import "./skill.css";
 
 const Skills = (props) => {
-  const skillLevel = props.value + '%';
+  const skillLevel = props.value + "%";
 
   return (
     <div className="skills-container">
       <div className="skills-details">
         <p>{props.name}</p>
-        <p>{props.value}%</p>
+        <p></p>
       </div>
       <div className="skill-level">
         <div className="skills-level-bar" style={{ width: skillLevel }}></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Skills;
