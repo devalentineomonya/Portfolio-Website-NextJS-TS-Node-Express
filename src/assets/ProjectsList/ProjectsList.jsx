599fc61da6099507c62ba5d0ad7d15f97034b6c4
Valentine Omonya <valomosh254@gmail.com>
2024-04-02 01:50:51 +0300

    Added Weather Project

--- src/assets/ProjectsList/ProjectsList.jsx
+++ src/assets/ProjectsList/ProjectsList.jsx
@@ -1,13 +1,14 @@
 import image1 from "../images/notAvailable.jpeg";
+import DevalWeatherApp from "../images/devalWeatherApp.png";
 let ProjectsList = [
   {
     id:1,
-    name: "Project 1 Name",
-    image: image1,
-    technology: "ReactJs",
-    description:"Aliqua nostrud non aute irure velit incididunt. Eu eiusmod ea officia incididunt proident aute eiusmod excepteur in cillum sunt elit do culpa. Ad esse deserunt anim reprehenderit ullamco fugiat aliqua proident elit esse. Fugiat quis laboris deserunt cillum. Elit ullamco voluptate irure minim.",
-    live_link: "http://reactjs.com",
-    github_link:"https://github.com/devalentineomonya"
+    name: "DevalWeather App",
+    image: DevalWeatherApp,
+    technology: "ReactJS | OpenWeather API",
+    description:"Plan your adventures with confidence thanks to the React Weather App! This user-friendly app provides hyperlocal weather forecasts for any location, giving you a detailed picture for the next five days in three-hour increments. Powered by open-source data, the app delivers accurate and up-to-date information on temperature, humidity, wind speed, and atmospheric pressure. Whether you're planning a hike, checking conditions at the beach, or just curious about the afternoon commute, the React Weather App has you covered. Effortless navigation allows you to quickly switch between locations, making it ideal for travelers and busy professionals. The app's intuitive interface presents weather data in a clear and organized way, ensuring you can grasp the information at a glance.",
+    live_link: "https://devalentineomonya.github.io/ReactJS-Weather-App/",
+    github_link:"https://github.com/devalentineomonya/ReactJS-Weather-App"
   },
   {
     id:2,
