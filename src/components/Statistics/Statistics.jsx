cfa235115985f3e65bdbd063d6d123dcdb282184
Valentine Omonya <valomosh254@gmail.com>
2024-07-04 22:12:48 +0300

    Added null check

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 import "./statistics.css";
-const Statistics = (props) => {
+const Statistics = ({value=0, name=""}) => {
   const [counterOn, setCounterOn] = useState(false);
   return (
     <div className="counter-item">
       <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
         <h1>
-          {counterOn && <CountUp start={0} end={props.value} duration={3} delay={0} />}
+          {counterOn && <CountUp start={0} end={value} duration={3} delay={0} />}
           
         </h1>
       </ScrollTrigger>
-      <p>{props.name}</p>
+      <p>{name}</p>
     </div>
   );
 };
