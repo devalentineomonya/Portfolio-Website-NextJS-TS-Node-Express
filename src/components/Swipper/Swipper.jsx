92bd97ac677729bd8db6306dd171b13c05e422ea
Valentine Omonya <valomosh254@gmail.com>
2024-07-04 22:13:47 +0300

    Added null check

--- src/components/Swipper/Swipper.jsx
+++ src/components/Swipper/Swipper.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./swipper.css"
-const Swipper = (props) => {
+const Swipper = (props=null) => {
   return (
     <div className='sliding-content'>
       <div className="swipper-image">
-        <img src={props.image} alt={props.name} />
+        <img src={props?.image} alt={props?.name} />
       </div>
       <div className="swipper-text">
-        <h2>{props.name}</h2>
-        <span>{props.position}</span>
-        <p>{props.description}</p>
+        <h2>{props?.name}</h2>
+        <span>{props?.position}</span>
+        <p>{props?.description}</p>
       </div>
     </div>
   )
