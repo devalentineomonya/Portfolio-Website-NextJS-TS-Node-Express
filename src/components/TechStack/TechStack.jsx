63245c915a3d729c14c665ae83fc6172786a7be4
Valentine Omonya <valomosh254@gmail.com>
2024-07-04 22:11:31 +0300

    Added null check

--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./techstack.css";
 import { BsChevronRight } from "react-icons/bs";
-const TechStacks = ({techstack}) => {
+const TechStacks = ({techstack=null}) => {
   const [isActive, setIsActive] = useState(false);
 
   const toggleAccordion = () => {
@@ -21,15 +21,15 @@ const TechStacks = ({techstack}) => {
     >
       <div className="card-content">
         <div className="techstack-icon">
-          {techstack.icon}
+          {techstack?.icon}
         </div>
         <div className="techstack-name">
           <BsChevronRight className="chevron"  size={35}/>
-          <p>{techstack.name}</p>
+          <p>{techstack?.name}</p>
         </div>
       </div>
       <div className="techstack-description">
-        <p>{techstack.description}</p>
+        <p>{techstack?.description}</p>
       </div>
     </div>
   );
