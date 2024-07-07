0ed786e63f5224d58a02acad7cc3b2df77d3c68c
Valentine Omonya <valomosh254@gmail.com>
2024-07-07 10:09:55 +0300

    disabled react deb tolols

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "./Context/ThemeContext.jsx";
 import DataContextProvider from "./Context/DataContext.jsx";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+if (import.meta.env.PROD) {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
