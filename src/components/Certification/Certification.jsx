02136296069412081b0c9dee2c3dca0474f686ba
Valentine Omonya <valomosh254@gmail.com>
2024-07-14 15:46:38 +0300

    Added zoom feaure for certificate section

--- src/components/Certification/Certification.jsx
+++ src/components/Certification/Certification.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import "./certification.css"
-
+import Zoom from "react-medium-image-zoom";
+import 'react-medium-image-zoom/dist/styles.css';
+import "./certification.css";
 
 const imageSource = import.meta.env.DEV
-? "http://localhost:8080/api/images"
-: `${import.meta.env.VITE_API_iconName}/images`;
-
+  ? "http://localhost:8080/api/images"
+  : `${import.meta.env.VITE_API_iconName}/images`;
 
 const Certification = ({ certification }) => {
   return (
     <div className="certification-card">
       <div className="certification-image">
-        <img src={`${imageSource}/${certification.image}`} alt={certification.name} />
+        <Zoom>
+          <img 
+            src={`${imageSource}/${certification.image}`} 
+            alt={certification.name} 
+            className="zoomable-image"
+          />
+        </Zoom>
       </div>
     </div>
   );
