fe51538cf5080856730d3f9551920e28bd93a589
Valentine Omonya <valomosh254@gmail.com>
2024-09-28 05:34:12 +0300

    Deleted vite project

--- src/Utils/EndPoints/EndPoints.jsx
+++
@@ -1,9 +0,0 @@
-export const apiEndpoint = import.meta.env.DEV
-? "http://localhost:8080/api"
-: import.meta.env.VITE_API_ENDPOINT;
-
-
-
-export const imageSource = import.meta.env.DEV
-? "http://localhost:8080/api/images"
-: `${import.meta.env.VITE_API_ENDPOINT}/images`;
