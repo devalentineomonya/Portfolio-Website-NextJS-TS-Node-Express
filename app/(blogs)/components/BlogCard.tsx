9cbb18d5cf6351a67122e9ad1733640ae09be24a
Valentine Omonya <valomosh254@gmail.com>
2024-10-07 21:06:21 +0300

    Added blog's page blue print

--- app/(blogs)/components/BlogCard.tsx
+++ app/(blogs)/components/BlogCard.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const BlogCard = () => {
   return (
     <div className="flex flex-col w-full max-h-[700px] h-full bg-red-700">
-      <div className="flex-1 w-full h-1/2">
+      <div className="flex-1 w-full h-1/2 relative">
         <Image
           src={blogImage}
           alt="Blog Image"
           layout="fill"
           quality={100}
           objectFit="cover"
-          className="absolute inset-0"
+          className="absolute inset-0 w-full h-full"
           placeholder="blur"
         />
       </div>
