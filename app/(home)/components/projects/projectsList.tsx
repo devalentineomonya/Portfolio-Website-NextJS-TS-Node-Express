f9f0f7a06c89eaec9df778d2d70603e7ba85c869
Valentine Omonya <valomosh254@gmail.com>
2024-10-23 23:26:21 +0300

    Added projects page and downgraded to next 14.2.16 after having dependancy conflicts

--- app/(home)/components/projects/projectsList.tsx
+++ app/(home)/components/projects/projectsList.tsx
@@ -3,7 +3,7 @@ import weatherPopup from "@/assets/images/devalWeatherApp2.png";
 import defaultPreview from "@/assets/images/defaultPreview.png";
 import defaultPopup from "@/assets/images/defaultPopup.png";
 //
-//SUGGESTED DEXCRIPTION LWNGTH 43 WORDS 295 CHARACTERS 
+//SUGGESTED DEXCRIPTION LWNGTH 43 WORDS 295 CHARACTERS
 // __________________________________________________//
 
 const projectsList = [
@@ -16,7 +16,8 @@ const projectsList = [
     description:
       "DevalShopping Cart is a full-stack eCommerce app built with ReactJS, NodeJS, and Express. It offers user login, product browsing, a shopping cart, secure checkout, and order tracking. With an intuitive interface, it provides a smooth and efficient shopping experience.",
     liveLink: "https://devalshoppingcart.vercel.app/",
-    githubLink: "https://github.com/devalentineomonya/React-Node-E-commerce-Application",
+    githubLink:
+      "https://github.com/devalentineomonya/React-Node-E-commerce-Application",
   },
 
   {
@@ -24,29 +25,31 @@ const projectsList = [
     name: "DevalCinemaz",
     previewImage: defaultPreview,
     popupImage: defaultPopup,
-    technologies: ["NextJS", "TMDB API", "Cleck Auth"],
+    technologies: ["Web", "NextJS", "TMDB API", "Cleck Auth"],
     description:
       "DevalCinemaz is a movie trailer platform built with NextJS and powered by the TMDB API. It lets users explore a vast collection of movies, view trailers, and access movie details like release dates, ratings, and overviews. With a responsive interface, users can browse through popular and upcoming films.",
     liveLink: "https://devalcinemaz.vercel.app/",
-    githubLink: "https://github.com/devalentineomonya/DevalCinemaz-NexJS-Movies-Site-Stable-Version",
+    githubLink:
+      "https://github.com/devalentineomonya/DevalCinemaz-NexJS-Movies-Site-Stable-Version",
   },
   {
     id: 3,
     name: "DevalMotors",
     previewImage: defaultPreview,
     popupImage: defaultPopup,
-    technologies: ["NextJS", "TS", "Hono", "NodeJS"],
+    technologies: ["Web", "NextJS", "TS", "Hono", "NodeJS"],
     description:
       "DevalMotors is a vehicle booking platform built with NextJS, TypeScript, Hono, and NodeJS. It allows users to browse and book various vehicles, offering a streamlined experience for scheduling rides and managing reservations. With a user-friendly interface and real-time booking features, DevalMotors makes vehicle rentals quick and efficient.",
     liveLink: "http://devalmotors.vercel.app",
-    githubLink: "https://github.com/devalentineomonya/Car-Rential-Website-Next-TS-Hono-React-Shadcn",
+    githubLink:
+      "https://github.com/devalentineomonya/Car-Rential-Website-Next-TS-Hono-React-Shadcn",
   },
   {
     id: 4,
     name: "DevalWeather App",
     previewImage: weatherPreview,
     popupImage: weatherPopup,
-    technologies: ["ReactJS","OpenWeather API"],
+    technologies: ["Web", "ReactJS", "OpenWeather API"],
     description:
       "The React Weather App provides hyperlocal, 5-day forecasts in 3-hour increments for any location. Access temperature, humidity, wind speed, and pressure data. Ideal for travelers and professionals, this app offers effortless navigation and a clear, intuitive interface for informed decision-making",
     liveLink: "https://deval-weather-app.vercel.app/",
@@ -57,7 +60,7 @@ const projectsList = [
     name: "254 Outfit E-Commerce",
     previewImage: defaultPreview,
     popupImage: defaultPopup,
-    technologies: ["NextJS","Hono", "TS", "Node"],
+    technologies: ["Web", "NextJS", "Hono", "TS", "Node"],
     description:
       "254 Outfit E-Commerce is an online platform built with NextJS, Hono, TypeScript, and NodeJS, designed to help users find stylish outfits in Kenya. With a focus on local fashion, the website offers a wide range of clothing options, easy navigation, and a seamless shopping experience.",
     liveLink: "https://254outfitcommerce.vercel.app",
@@ -68,7 +71,7 @@ const projectsList = [
     name: "DevalCodeMe",
     previewImage: defaultPreview,
     popupImage: defaultPopup,
-    technologies: ["NextJS","Hono", "TS", "Node"],
+    technologies: ["Web", "NextJS", "Hono", "TS", "Node"],
     description:
       "DevalCodeMe is an e-learning platform built with NextJS, Hono, TypeScript, and NodeJS. It offers coding challenges, tests, and learning resources to help users improve their programming skills in a practical and engaging way.",
     liveLink: "https://devalcodeme.vercel.app",
@@ -76,6 +79,3 @@ const projectsList = [
   },
 ];
 export default projectsList;
-
-
-
--- app/(projects)/projects/page.tsx
+++ app/(projects)/projects/page.tsx
@@ -1,11 +1,131 @@
-import React from 'react'
+"use client";
+import { useEffect, useRef, useState } from "react";
+import Isotope from "isotope-layout";
+import projectsList from "@/app/(home)/components/projects/projectsList";
+import Image from "next/image";
 
-const page = () => {
+const Gallery = () => {
+  const gridRef = useRef<HTMLDivElement | null>(null);
+  const isoRef = useRef<Isotope | null>(null);
+
+  const [selectedStack, setSelectedStack] = useState<string>("");
+  const [selectedEnvironment, setSelectedEnvironment] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && gridRef.current) {
+      isoRef.current = new Isotope(gridRef.current, {
+        itemSelector: ".grid-item",
+        layoutMode: "fitRows",
+        percentPosition: true,
+      });
+    }
+
+    return () => {
+      if (isoRef.current) {
+        isoRef.current.destroy();
+      }
+    };
+  }, []);
+
+  const handleFilter = () => {
+    const filterValue =
+      `${selectedStack ? `.${selectedStack}` : ""}${
+        selectedEnvironment ? `.${selectedEnvironment}` : ""
+      }` || "*";
+    if (isoRef.current) {
+      isoRef.current.arrange({ filter: filterValue });
+    }
+  };
+
+  const getFilterClassName = (technologies: string[]) => {
+    return technologies
+      .map((tech) => tech.replace(/\s+/g, "").toLowerCase())
+      .join(" ");
+  };
+
   return (
-    <div className="h-[calc(100vh-6rem)] flex items-center justify-center  py-12">
-      <h1 className="text-5xl font-medium">Coming Soon</h1>
-    </div>
-  )
-}
+    <>
+      <div className="mb-4 space-x-4">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedStack("");
+            setSelectedEnvironment("");
+            handleFilter();
+          }}
+        >
+          Show All
+        </button>
+        {/* Hardcoded filter buttons */}
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedStack("reactjs");
+            handleFilter();
+          }}
+        >
+          ReactJS
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedStack("nodejs");
+            handleFilter();
+          }}
+        >
+          NodeJS
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedStack("nextjs");
+            handleFilter();
+          }}
+        >
+          NextJS
+        </button>
 
-export default page
+        {/* Environment filters */}
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedEnvironment("web");
+            handleFilter();
+          }}
+        >
+          Web
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={() => {
+            setSelectedEnvironment("mobile");
+            handleFilter();
+          }}
+        >
+          Mobile
+        </button>
+      </div>
+
+      <div className="flex items-start justify-center h-fit min-h-[calc(100vh-6rem)] py-12">
+        <div className="grid grid-cols-4 gap-8 max-w-[1350px]" ref={gridRef}>
+          {projectsList.map((project) => (
+            <div
+              key={project.id}
+              className={`${getFilterClassName(project.technologies)} ${
+                project.name.includes("Mobile") ? "mobile" : "web"
+              } rounded shadow grid-item`}
+            >
+              <Image
+                src={project.popupImage}
+                alt={project.name}
+                className="w-full aspect-square object-cover mb-2 rounded"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
