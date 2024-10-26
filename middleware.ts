4ebde6e36b24bf7d92775f1e528c8242d44b5c57
Valentine Omonya <valomosh254@gmail.com>
2024-10-26 07:44:42 +0300

    Added middleware config

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,25 @@
-export { auth as middleware } from "@/config/nextAuth";
+
+import { NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req }); 
+
+  const requestedPath = req.nextUrl.pathname;
+
+  if (token) {
+    if (requestedPath === '/admin/auth/signin') {
+      return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+    }
+  } else {
+    if (!requestedPath.startsWith('/admin/auth')) {
+      return NextResponse.redirect(new URL('/admin/auth/signin', req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/admin/:path*"],
