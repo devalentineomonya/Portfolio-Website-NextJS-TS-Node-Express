b40cb2decb551ac5483c7ac2c2948436a51d3cf5
Valentine Omonya <valomosh254@gmail.com>
2024-11-02 16:26:47 +0300

    Added use server directive trying to resolve env not found bug

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,7 +1,7 @@
 "use server"
 import {migrate} from "drizzle-orm/vercel-postgres/migrator";
 import { db }  from "@/drizzle/db";
-async function main() {
+export async function main() {
     await migrate(db , {migrationsFolder: "./drizzle/migrations"});
 }
 
